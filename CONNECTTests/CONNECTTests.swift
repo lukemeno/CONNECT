37d671a8476005f0b2cd7a74d7da78a7071e82e8
lukemeno <lukemeno@gmail.com>
2025-07-19 19:21:05 +0200

    Initial Commit

---
+++ CONNECTTests/CONNECTTests.swift
@@ -0,0 +1,17 @@
+//
+//  CONNECTTests.swift
+//  CONNECTTests
+//
+//  Created by Luke on 19.07.25.
+//
+
+import Testing
+@testable import CONNECT
+
+struct CONNECTTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
